bb3fdd50e524ce242494f4b23310a3260ba70936
chenDoInG <chendoing@gmail.com>
2015-10-03 13:27:36 +0800

    add container with most water

---
+++ src/com/chendoing/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,41 @@
+package com.chendoing.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Given n non-negative integers a1, a2, ...,
+ * an, where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container,
+ * such that the container contains the most water.
+ * Note: You may not slant the container.
+ * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ * <p>
+ * Note: You may not slant the container.
+ */
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right])
+                    * (right - left));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+
+    @Test
+    public void maxArea() {
+        int[] heigh = {5, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        System.out.print(maxArea(heigh));
+    }
+}
