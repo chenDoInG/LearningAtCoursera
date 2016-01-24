c357aaa3d2136cc97e66bbd202a2c310d6808e47
chenDoInG <chendoing@gmail.com>
2016-01-24 20:45:49 +0800

    add dynamic programming

--- src/com/chendoing/leetcode/LongestValidParentheses.java
+++ src/com/chendoing/leetcode/LongestValidParentheses.java
@@ -33,8 +33,35 @@ public class LongestValidParentheses {
         return max;
     }
 
+    /**
+     * 使用matched数组来记录已经匹配的长度,
+     * 计算下一个右括号')'时,寻找的左括号'('的位置left=right-1-matched[right-1],
+     * 如果左括号'('的位置小于0计算结束,
+     * 如果左括号'('左边位置left-1大于左边界的话,match[right]+=match[left-1]
+     * s:       ) ( ) ( ) )
+     * index:   0 1 2 3 4 5
+     * matched: 0 0 2 0 4 0
+     */
+    public int longestValidParenthesesByDp(String s) {
+        int n = s.length(), max = 0;
+        int[] matched = new int[n];
+        for (int right = 1; right < n; right++) {
+            if (s.charAt(right) == ')') {
+                int left = right - 1 - matched[right - 1];
+                if (left >= 0 && s.charAt(left) == '(') {
+                    matched[right] = matched[right - 1] + 2;
+                    if (left - 1 > 0)
+                        matched[right] += matched[left - 1];
+                }
+            }
+            max = max <= matched[right] ? matched[right] : max;
+        }
+        return max;
+    }
+
     @Test
     public void longestValidParentheses() {
-        System.out.println(longestValidParentheses(")()(())"));
+        System.out.println(longestValidParentheses(")()(())))"));
+        System.out.println(longestValidParenthesesByDp("(()))()()"));
     }
 }
